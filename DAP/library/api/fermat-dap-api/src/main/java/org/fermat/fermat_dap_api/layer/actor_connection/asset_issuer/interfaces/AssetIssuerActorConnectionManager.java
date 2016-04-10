c9104442c97008bf503f89e71de1fdc45732f656
acostarodrigo <acosta_rodrigo@hotmail.com>
2016-04-10 18:44:53 -0300

    Added ActorConnection interfaces for DAP actors

---
+++ DAP/library/api/fermat-dap-api/src/main/java/org/fermat/fermat_dap_api/layer/actor_connection/asset_issuer/interfaces/AssetIssuerActorConnectionManager.java
@@ -0,0 +1,10 @@
+package org.fermat.fermat_dap_api.layer.actor_connection.asset_issuer.interfaces;
+
+
+import com.bitdubai.fermat_api.layer.actor_connection.common.interfaces.ActorConnectionManager;
+
+/**
+ * Created by rodrigo on 4/10/16.
+ */
+public interface AssetIssuerActorConnectionManager extends ActorConnectionManager{
+}
---
+++ DAP/library/api/fermat-dap-api/src/main/java/org/fermat/fermat_dap_api/layer/actor_connection/redeem_point/interfaces/RedeemPointActorConnectionManager.java
@@ -0,0 +1,9 @@
+package org.fermat.fermat_dap_api.layer.actor_connection.redeem_point.interfaces;
+
+import com.bitdubai.fermat_api.layer.actor_connection.common.interfaces.ActorConnectionManager;
+
+/**
+ * Created by rodrigo on 4/10/16.
+ */
+public interface RedeemPointActorConnectionManager extends ActorConnectionManager {
+}
