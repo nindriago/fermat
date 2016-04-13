c49cc34e2d7ab6a2b5cdcc5e178264656f7aa09b
Víctor A. Mars M <marsvicam@gmail.com>
2016-04-12 23:43:05 -0430

    IMPLEMENTATION: Update public exceptions for Digital Asset Metadata Plugin
    Associated Issue: #5919

--- DAP/library/api/fermat-dap-api/src/main/java/org/fermat/fermat_dap_api/layer/metadata/exceptions/CantGetDigitalAssetMetadataException.java
+++ DAP/library/api/fermat-dap-api/src/main/java/org/fermat/fermat_dap_api/layer/metadata/exceptions/CantGetDigitalAssetMetadataException.java
@@ -6,4 +6,26 @@ import org.fermat.fermat_dap_api.layer.all_definition.exceptions.DAPException;
  * Created by rodrigo on 4/10/16.
  */
 public class CantGetDigitalAssetMetadataException extends DAPException {
+    public CantGetDigitalAssetMetadataException(String message, Exception cause, String context, String possibleReason) {
+        super(message, cause, context, possibleReason);
+    }
+
+    public CantGetDigitalAssetMetadataException(Exception cause, String context, String possibleReason) {
+        super(cause, context, possibleReason);
+    }
+
+    public CantGetDigitalAssetMetadataException(String message, Exception cause) {
+        super(message, cause);
+    }
+
+    public CantGetDigitalAssetMetadataException(String message) {
+        super(message);
+    }
+
+    public CantGetDigitalAssetMetadataException(Exception exception) {
+        super(exception);
+    }
+
+    public CantGetDigitalAssetMetadataException() {
+    }
 }
