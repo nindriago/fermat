1db5033d825cfceb6b8daa97b29e354f0bab7a3a
Penelope <pennyxz@gmail.com>
2016-04-18 17:53:44 -0430

    Changes in #6052

--- DAP/android/reference_wallet/fermat-dap-android-reference-wallet-redeem-point/src/main/java/org/fermat/fermat_dap_android_wallet_redeem_point/v3/fragments/RedeemPointDetailFragment.java
+++ DAP/android/reference_wallet/fermat-dap-android-reference-wallet-redeem-point/src/main/java/org/fermat/fermat_dap_android_wallet_redeem_point/v3/fragments/RedeemPointDetailFragment.java
@@ -1,7 +1,7 @@
 package org.fermat.fermat_dap_android_wallet_redeem_point.v3.fragments;
 
 /**
- * Created by penny on 18/04/16.
+ * Created by Penny on 18/04/16.
  */
 public class RedeemPointDetailFragment {
 }
