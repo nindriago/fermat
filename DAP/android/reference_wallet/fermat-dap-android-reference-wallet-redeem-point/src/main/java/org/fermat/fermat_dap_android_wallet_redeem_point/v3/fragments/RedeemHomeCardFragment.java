26cadf4c1cb1027165e5e213d743319d7521f73d
jinmyjbv <jinmybohorquez@gmail.com>
2016-04-19 03:58:59 -0430

    DAP - Reference Wallet - Asset Redeem Point - Redeem Point Home - Android New GUI DAP #777

--- DAP/android/reference_wallet/fermat-dap-android-reference-wallet-redeem-point/src/main/java/org/fermat/fermat_dap_android_wallet_redeem_point/v3/fragments/RedeemHomeCardFragment.java
+++ DAP/android/reference_wallet/fermat-dap-android-reference-wallet-redeem-point/src/main/java/org/fermat/fermat_dap_android_wallet_redeem_point/v3/fragments/RedeemHomeCardFragment.java
@@ -1,7 +1,69 @@
 package org.fermat.fermat_dap_android_wallet_redeem_point.v3.fragments;
 
+import android.support.v7.widget.RecyclerView;
+
+import com.bitdubai.fermat_android_api.ui.adapters.FermatAdapter;
+import com.bitdubai.fermat_android_api.ui.fragments.FermatWalletListFragment;
+import com.bitdubai.fermat_android_api.ui.interfaces.FermatListItemListeners;
+import com.bitdubai.fermat_dap_android_wallet_redeem_point_bitdubai.R;
+
+import org.fermat.fermat_dap_android_wallet_redeem_point.models.DigitalAsset;
+
 /**
- * Created by madscientist on 15/04/16.
+ * Created by Jinmy Bohorquez on 15/04/16.
  */
-public class RedeemHomeCardFragment {
+public class RedeemHomeCardFragment extends FermatWalletListFragment<DigitalAsset> implements FermatListItemListeners<DigitalAsset> {
+
+    @Override
+    protected boolean hasMenu() {
+        return false;
+    }
+
+    @Override
+    protected int getLayoutResource() {return R.layout.dap_v3_wallet_asset_redeem_point_home_card;}
+
+    @Override
+    protected int getSwipeRefreshLayoutId() {
+        return R.id.redeem_swipe_refresh;
+    }
+
+    @Override
+    protected int getRecyclerLayoutId() {
+        return R.id.dap_v3_wallet_asset_redeem_home_recycler_view;
+    }
+
+    @Override
+    protected boolean recyclerHasFixedSize() {
+        return false;
+    }
+
+    @Override
+    public void onPostExecute(Object... result) {
+
+    }
+
+    @Override
+    public void onErrorOccurred(Exception ex) {
+
+    }
+
+    @Override
+    public FermatAdapter getAdapter() {
+        return null;
+    }
+
+    @Override
+    public RecyclerView.LayoutManager getLayoutManager() {
+        return null;
+    }
+
+    @Override
+    public void onItemClickListener(DigitalAsset data, int position) {
+
+    }
+
+    @Override
+    public void onLongItemClickListener(DigitalAsset data, int position) {
+
+    }
 }
