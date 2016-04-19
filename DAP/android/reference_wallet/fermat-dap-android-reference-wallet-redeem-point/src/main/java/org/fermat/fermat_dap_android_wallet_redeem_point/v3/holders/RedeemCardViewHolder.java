23bf83cb181aebb4a0411a1b900a377c0646acd0
jinmyjbv <jinmybohorquez@gmail.com>
2016-04-19 03:58:50 -0430

    DAP - Reference Wallet - Asset Redeem Point - Redeem Point Home - Android New GUI DAP #777

---
+++ DAP/android/reference_wallet/fermat-dap-android-reference-wallet-redeem-point/src/main/java/org/fermat/fermat_dap_android_wallet_redeem_point/v3/holders/RedeemCardViewHolder.java
@@ -0,0 +1,23 @@
+package org.fermat.fermat_dap_android_wallet_redeem_point.v3.holders;
+
+import android.content.Context;
+import android.view.View;
+
+import com.bitdubai.fermat_android_api.ui.holders.FermatViewHolder;
+
+import org.fermat.fermat_dap_api.layer.dap_module.wallet_asset_redeem_point.interfaces.AssetRedeemPointWalletSubAppModule;
+
+
+/**
+ * Created by Jinmy Bohorquez on 19/04/16.
+ */
+public class RedeemCardViewHolder extends FermatViewHolder {
+
+    public RedeemCardViewHolder(View itemView, AssetRedeemPointWalletSubAppModule manager, Context context) {
+        super(itemView);
+    }
+
+
+
+
+}
