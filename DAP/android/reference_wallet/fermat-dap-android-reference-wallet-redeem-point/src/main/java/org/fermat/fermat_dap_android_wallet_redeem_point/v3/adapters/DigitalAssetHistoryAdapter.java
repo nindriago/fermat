87cbb0c6ba7c155d27a0bd8bb2d92e6bb6f82db0
Penelope <pennyxz@gmail.com>
2016-04-19 20:11:46 -0430

    Changes in #6052 adapter and holder of new dapv3 redeem point history tab

---
+++ DAP/android/reference_wallet/fermat-dap-android-reference-wallet-redeem-point/src/main/java/org/fermat/fermat_dap_android_wallet_redeem_point/v3/adapters/DigitalAssetHistoryAdapter.java
@@ -0,0 +1,80 @@
+package org.fermat.fermat_dap_android_wallet_redeem_point.v3.adapters;
+
+import android.content.Context;
+import android.view.View;
+
+import com.bitdubai.fermat_android_api.ui.adapters.FermatAdapter;
+import com.bitdubai.fermat_dap_android_wallet_redeem_point_bitdubai.R;
+
+import org.fermat.fermat_dap_android_wallet_redeem_point.v3.holders.DigitalAssetHistoryItemViewHolder;
+import org.fermat.fermat_dap_android_wallet_redeem_point.v3.models.DigitalAssetHistory;
+import org.fermat.fermat_dap_api.layer.dap_module.wallet_asset_redeem_point.interfaces.AssetRedeemPointWalletSubAppModule;
+
+import java.sql.Timestamp;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.LinkedHashMap;
+import java.util.List;
+import java.util.Set;
+
+/**
+ * Created by Penny on 19/04/16.
+ */
+public class DigitalAssetHistoryAdapter extends FermatAdapter<DigitalAssetHistory, DigitalAssetHistoryItemViewHolder> {
+
+    private AssetRedeemPointWalletSubAppModule manager;
+    private LinkedHashMap<Timestamp, Integer> mapIndex;
+    private LinkedHashMap<Timestamp, Integer> mapAssetQuantity;
+    private ArrayList<Timestamp> sectionList;
+    private String[] sections;
+
+    protected DigitalAssetHistoryAdapter(Context context, List<DigitalAssetHistory> dataSet, AssetRedeemPointWalletSubAppModule manager) {
+        super(context, dataSet);
+        this.manager = manager;
+        fillSections();
+    }
+
+    private void fillSections() {
+        mapIndex = new LinkedHashMap<Timestamp, Integer>();
+
+        //Sort dataSet for accurate date display
+        Collections.sort(dataSet);
+        for (int x = 0; x < dataSet.size(); x++) {
+            Timestamp dateAccepted = dataSet.get(x).getAcceptedDate();
+            if (dateAccepted != null) {
+                if (!mapIndex.containsKey(dateAccepted)) {
+                    mapIndex.put(dateAccepted, x);
+                    mapAssetQuantity.put(dateAccepted,0);
+                }else {
+                    mapAssetQuantity.put(dateAccepted, mapAssetQuantity.get(dateAccepted) + 1);
+                }
+            }
+        }
+        Set<Timestamp> sectionDates = mapIndex.keySet();
+        // create a list from the set to sort
+        sectionList = new ArrayList<>(sectionDates);
+        Collections.sort(sectionList);
+
+        sections = new String[sectionList.size()];
+        sectionList.toArray(sections);
+    }
+
+    private Timestamp getSection(DigitalAssetHistory digitalAssetHistory) {
+        return digitalAssetHistory.getAcceptedDate();
+    }
+
+    @Override
+    protected DigitalAssetHistoryItemViewHolder createHolder(View itemView, int type) {
+        return new DigitalAssetHistoryItemViewHolder(itemView, manager, context);
+    }
+
+    @Override
+    protected int getCardViewResource() {
+        return R.layout.dap_v3_wallet_asset_redeem_point_asset_user_history_item;
+    }
+
+    @Override
+    protected void bindHolder(DigitalAssetHistoryItemViewHolder holder, DigitalAssetHistory data, int position) {
+        holder.bind(data, data.getFormattedAcceptedDate(), mapAssetQuantity.get(data.getAcceptedDate()) , mapIndex.get(data.getAcceptedDate()) == position);
+    }
+}
---
+++ DAP/android/reference_wallet/fermat-dap-android-reference-wallet-redeem-point/src/main/java/org/fermat/fermat_dap_android_wallet_redeem_point/v3/holders/DigitalAssetHistoryItemViewHolder.java
@@ -0,0 +1,86 @@
+package org.fermat.fermat_dap_android_wallet_redeem_point.v3.holders;
+
+import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+
+import com.bitdubai.fermat_android_api.layer.definition.wallet.utils.ImagesUtils;
+import com.bitdubai.fermat_android_api.layer.definition.wallet.views.FermatTextView;
+import com.bitdubai.fermat_android_api.ui.holders.FermatViewHolder;
+import com.bitdubai.fermat_dap_android_wallet_redeem_point_bitdubai.R;
+
+import org.fermat.fermat_dap_android_wallet_redeem_point.models.UserRedeemed;
+import org.fermat.fermat_dap_android_wallet_redeem_point.v3.models.DigitalAssetHistory;
+import org.fermat.fermat_dap_api.layer.dap_module.wallet_asset_redeem_point.interfaces.AssetRedeemPointWalletSubAppModule;
+
+/**
+ * Created by Penny on 19/04/16.
+ */
+public class DigitalAssetHistoryItemViewHolder extends FermatViewHolder {
+    private AssetRedeemPointWalletSubAppModule manager;
+    private Context context;
+
+    private final LinearLayout historySectionHeader;
+    private final FermatTextView historyDateHeader;
+    private final FermatTextView historyAssetsQuantity;
+
+    private final FermatTextView historyAssetName;
+    private final FermatTextView historyUserName;
+
+    private final ImageView imageViewAssetRedeemedAvatar;
+    private final ImageView imageViewUserRedeemedAvatar;
+
+    /**
+     * Constructor
+     *
+     * @param itemView
+     */
+    public DigitalAssetHistoryItemViewHolder(View itemView, AssetRedeemPointWalletSubAppModule manager, Context context) {
+        super(itemView);
+        this.manager = manager;
+        this.context = context;
+
+        historySectionHeader = (LinearLayout) itemView.findViewById(R.id.historySectionHeader);
+
+        historyDateHeader = (FermatTextView) itemView.findViewById(R.id.historyDateHeader);
+        historyAssetsQuantity = (FermatTextView) itemView.findViewById(R.id.historyAssetsQuantity);
+        historyAssetName = (FermatTextView) itemView.findViewById(R.id.historyAssetName);
+        historyUserName = (FermatTextView) itemView.findViewById(R.id.historyUserName);
+
+        imageViewAssetRedeemedAvatar = (ImageView) itemView.findViewById(R.id.redeemedListUserName);
+        imageViewUserRedeemedAvatar = (ImageView) itemView.findViewById(R.id.redeemedListRedeemedDate);
+
+    }
+
+    public void bind(final DigitalAssetHistory digitalAssetHistory, String sectionTextDate, Integer assetsQuantity, boolean showSection) {
+
+        if (showSection)
+        {
+            historySectionHeader.setVisibility(View.VISIBLE);
+            historyDateHeader.setText(sectionTextDate);
+            historyAssetsQuantity.setText(assetsQuantity > 1 ? (assetsQuantity+" Assets"):(assetsQuantity+" Asset"));
+        }
+        else
+            historySectionHeader.setVisibility(View.GONE);
+
+        historyAssetName.setText(digitalAssetHistory.getHistoryNameAsset());
+        historyUserName.setText(digitalAssetHistory.getHistoryNameUser());
+
+        if (digitalAssetHistory.getImageAsset() != null && digitalAssetHistory.getImageAsset().length > 0) {
+            Bitmap bitmap = BitmapFactory.decodeByteArray(digitalAssetHistory.getImageAsset(), 0, digitalAssetHistory.getImageAsset().length);
+            bitmap = Bitmap.createScaledBitmap(bitmap, 120, 120, true);
+            imageViewAssetRedeemedAvatar.setImageDrawable(ImagesUtils.getRoundedBitmap(context.getResources(), bitmap));
+        }
+
+        if (digitalAssetHistory.getImageActorUserFrom() != null && digitalAssetHistory.getImageActorUserFrom().length > 0) {
+            Bitmap bitmap = BitmapFactory.decodeByteArray(digitalAssetHistory.getImageActorUserFrom(), 0, digitalAssetHistory.getImageActorUserFrom().length);
+            bitmap = Bitmap.createScaledBitmap(bitmap, 120, 120, true);
+            imageViewUserRedeemedAvatar.setImageDrawable(ImagesUtils.getRoundedBitmap(context.getResources(), bitmap));
+        }
+    }
+
+
+}
